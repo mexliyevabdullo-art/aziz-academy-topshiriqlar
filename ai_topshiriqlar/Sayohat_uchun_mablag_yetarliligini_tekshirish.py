4e4f351ec042d95750aaa6ea4708d5e1e5426df4
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-06-16 08:32:44 +0500

    Add AI task: Sayohat uchun mablag' yetarliligini tekshirish

---
+++ ai_topshiriqlar/Sayohat_uchun_mablag_yetarliligini_tekshirish.py
@@ -0,0 +1,12 @@
+# Sayohat uchun mablag' yetarliligini tekshirish
+# Kurs: Dasturlash / IT
+# Mavzu: Ma’lumot turlari: bool va mantiqiy ifodalar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+mablag = int(input())
+if mablag >= 5000:
+    print("yetarli")
+else:
+    print("yetarli emas")
