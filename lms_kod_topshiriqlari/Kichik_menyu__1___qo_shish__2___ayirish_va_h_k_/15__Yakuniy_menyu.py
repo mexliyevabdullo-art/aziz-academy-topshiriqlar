2a27534156e23ae9303925d2bc95b04bea4752e2
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-06-19 10:15:24 +0500

    Add: 15) Yakuniy menyu [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/15__Yakuniy_menyu.py
@@ -0,0 +1,19 @@
+while True:
+    s = input()
+    if s == "0":
+        print("Exit")
+        break
+    a, b = map(int, s.split())
+    amal = int(input())
+    if amal == 1:
+        print(a + b)
+    elif amal == 2:
+        print(a - b)
+    elif amal == 3:
+        print(a * b)
+    elif amal == 4:
+        print(a / b)
+    elif amal == 5:    
+        print(a % b)
+    elif amal == 6:
+        print(a ** b)
