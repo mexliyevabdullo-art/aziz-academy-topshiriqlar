de12b47de306a246a4e8a28f1504d8532f1d314d
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-07-02 13:13:35 +0500

    Add: Akkaunt holati bayroqlari [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
@@ -0,0 +1,6 @@
+royhatdan_otgan = True
+email_tasdiqlangan = False
+bloklangan = False
+print("Ro'yhatdan o'tgan:", royhatdan_otgan)
+print("Email tasdiqlangan:", email_tasdiqlangan)
+print("Bloklangan:", bloklangan)
