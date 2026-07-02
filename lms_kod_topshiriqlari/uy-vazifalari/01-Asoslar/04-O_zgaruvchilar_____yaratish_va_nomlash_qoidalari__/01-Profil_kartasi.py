6863c74c21efcb9fa58a5b85fc21cd4e5daf7d60
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-07-02 12:04:32 +0500

    Add: Profil kartasi [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -0,0 +1,10 @@
+ism = "AZIZ"
+familiya = "Karimov"
+kurs = "Python"
+daraja = "Boshlang'ich"
+shahar = "Toshkent"
+print("=== PROFIL ===")
+print("Ism:", ism)
+print("Familiya:", familiya)
+print("Kurs:", daraja)
+print("Shahar:", shahar)
