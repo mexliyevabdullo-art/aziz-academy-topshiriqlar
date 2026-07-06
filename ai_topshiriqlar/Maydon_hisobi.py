013e14a231a5d779396cb56dbca54305c934bf1a
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-07-06 09:32:18 +0500

    Add AI task: Maydon hisobi

---
+++ ai_topshiriqlar/Maydon_hisobi.py
@@ -0,0 +1,12 @@
+# Maydon hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+print(A * B - C * D)
+print(2 * (A + B))
