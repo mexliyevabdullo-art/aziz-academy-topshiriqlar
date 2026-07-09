014185a3d1dffe99dacb94c58189b57ff69b7e7c
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-07-09 09:29:21 +0500

    Add: Xatosiz to'liq hisob [mexliyevabdullo-art]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/15-Xatolik_turlari_____syntax_vs_runtime__xatoni_o_qi/01-Xatosiz_to_liq_hisob.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/15-Xatolik_turlari_____syntax_vs_runtime__xatoni_o_qi/01-Xatosiz_to_liq_hisob.py
@@ -1,9 +1,6 @@
 a = int(input())
 b = int(input())
 c = int(input())
-natija1 = (a + b) * c
-natija2 = a % c
-natija3 = a / b
-print(natija1)
-print(natija2)
-print(natija3)
+print((a + b) * c)
+print(a % c)
+print(a / b)
