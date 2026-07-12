e5185ef5d6e442f1fb3abf9930f4f88849e8be5e
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-07-12 12:30:24 +0500

    Add: O'lchov birliklari konvertori [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-O_lchov_birliklari_konvertori.py
@@ -0,0 +1,10 @@
+tur = int(input())
+qiymat = int(input())
+if tur == 1:
+    print(qiymat * 1000)
+elif tur == 2:
+    print(qiymat * 60)
+elif tur == 3:
+    print(qiymat * 1000)
+else:
+    print("Notogri tanlov")
