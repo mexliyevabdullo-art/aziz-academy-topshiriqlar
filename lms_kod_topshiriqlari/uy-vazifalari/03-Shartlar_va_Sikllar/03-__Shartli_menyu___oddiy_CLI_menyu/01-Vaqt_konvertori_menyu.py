c09f29b16f5c023c7ee1e96605a07fbe7bb28cbb
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-07-17 09:58:09 +0500

    Add: Vaqt konvertori menyu [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Vaqt_konvertori_menyu.py
@@ -0,0 +1,12 @@
+menyu = int(input())
+n = int(input())
+if menyu == 1:
+    minut = n // 60
+    soniya = n % 60
+    print(f"{minut} minut {soniya} soniya")
+elif menyu == 2:
+    soat = n // 60
+    minut = n % 60
+    print(f"{soat} soat {minut} minut")
+else:
+    print("Notogri tanlov")
