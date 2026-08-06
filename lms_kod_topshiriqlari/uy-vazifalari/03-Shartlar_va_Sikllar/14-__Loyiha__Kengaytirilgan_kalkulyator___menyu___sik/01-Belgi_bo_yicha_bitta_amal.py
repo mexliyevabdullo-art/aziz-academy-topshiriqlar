c8efc1fc3a274027251dc753c807ede0112e534f
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-08-06 21:48:51 +0500

    Add: Belgi bo'yicha bitta amal [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Belgi_bo_yicha_bitta_amal.py
@@ -0,0 +1,11 @@
+a = int(input())
+belgi = input().strip()
+b = int(input())
+if belgi == "+":
+    print(a + b)
+elif belgi == "-":
+    print(a - b)
+elif belgi == "*":
+    print(a * b)
+else:
+    print("Noma'lum amal")
