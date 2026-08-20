e7530f8b6a01ddb58c1dc3dfae6de7f3c8dc2003
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-08-20 20:46:21 +0500

    Add: Ikki nuqta orasidagi masofa kvadrati [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/04-Ma_lumot_Tuzilmalari/05-Tuple___o_zgarmas_tuzilma__qachon_kerak/01-Ikki_nuqta_orasidagi_masofa_kvadrati.py
@@ -0,0 +1,8 @@
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+p1 = (x1, y1)
+p2 = (x2, y2)
+distance_squared = (p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2
+print(distance_squared)
