2ae9cd75f80fdc275eed4ceda003c67fc97f5df9
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-08-21 11:33:23 +0500

    Add: 10) Eng qimmat mahsulot narxi [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/12-List_of_dicts_____o_quvchilar_mahsulotlar_ro_yxati/10-10__Eng_qimmat_mahsulot_narxi.py
@@ -0,0 +1,11 @@
+# N ta mahsulot
+# Eng qimmat mahsulot narxini chiqaring
+
+n = int(input())
+products = []
+for _ in range(n):
+    name, price = input().split()
+    products.append({'name': name, 'price': int(price)})
+# TODO
+max_price = max(p['price'] for p in products)
+print(max_price)
