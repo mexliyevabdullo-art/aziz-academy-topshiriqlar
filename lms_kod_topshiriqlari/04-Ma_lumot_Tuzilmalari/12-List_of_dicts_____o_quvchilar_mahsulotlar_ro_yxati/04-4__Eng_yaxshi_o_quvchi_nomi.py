6f1b94fbe60fc701d28b454f43d048f7b05d1339
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-08-21 11:02:32 +0500

    Add: 4) Eng yaxshi o‘quvchi nomi [mexliyevabdullo-art]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/12-List_of_dicts_____o_quvchilar_mahsulotlar_ro_yxati/04-4__Eng_yaxshi_o_quvchi_nomi.py
@@ -0,0 +1,8 @@
+
+n = int(input())
+students = []
+for _ in range(n):
+    name, score = input().split()
+    students.append({'name': name, 'score': int(score)})
+best_student = max(students, key=lambda x: x['score'])
+print(best_student['name'])
