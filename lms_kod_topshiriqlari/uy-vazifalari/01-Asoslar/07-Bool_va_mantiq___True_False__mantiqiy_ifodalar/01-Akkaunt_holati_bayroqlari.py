594496d1c0f5cd84189b3e366b41d9e34c05b99c
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-08-27 20:32:15 +0500

    Add: Akkaunt holati bayroqlari [mexliyevabdullo-art]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
@@ -1,6 +1,6 @@
 royxatdan_otgan = True
 email_tasdiqlangan = False
 bloklangan = False
-print("Ro'yxatdan o'tgan:", royxatdan_otgan)
-print("Email tasdiqlangan:", email_tasdiqlangan)
-print("Bloklangan:", bloklangan)
+print(f"Ro'yxatdan o'tgan: {royxatdan_otgan}")
+print(f"Email tasdiqlangan: {email_tasdiqlangan}")
+print(f"Bloklangan: {bloklangan}")
