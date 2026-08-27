4aaa20e5773a5d1f7af015d390973497e5fb4467
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-08-27 13:42:27 +0500

    Add: Uchta so'z uzunligi [mexliyevabdullo-art]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/01-Uchta_so_z_uzunligi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/01-Uchta_so_z_uzunligi.py
@@ -1,6 +1,3 @@
-a = "Python"
-b = "kod"
-c = "akademiya"
-print(a, "->", len(a))
-print(b, "->", len(b))
-print(c, "->", len(c))
+print("Python", "->", len("Python"))
+print("kod", "->", len("kod"))
+print("akademiya", "->", len("akademiya"))
