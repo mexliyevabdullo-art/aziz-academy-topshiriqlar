eaaf01627649de3755a5be4b3c2d3ba589afa880
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-09-08 21:32:29 +0500

    Add: Chegirmali narx (.format) [mexliyevabdullo-art]

--- lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/21-Boshqa_formatlash____format___va____eski_usullar_/01-Chegirmali_narx___format_.py
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/21-Boshqa_formatlash____format___va____eski_usullar_/01-Chegirmali_narx___format_.py
@@ -1,4 +1,4 @@
 narx = int(input())
 chegirma = int(input())
-yakuniy = narx * (100 - chegirma) // 100
-print("Yakuniy narx: {} so'm".format(yakuniy))
+yakuniy_narx = narx * (100 - chegirma) // 100
+print("Yakuniy narx: {} so'm".format(yakuniy_narx))
