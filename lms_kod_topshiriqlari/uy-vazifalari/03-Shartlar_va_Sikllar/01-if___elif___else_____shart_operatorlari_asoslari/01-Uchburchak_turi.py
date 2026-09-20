2bda2a96c4571becadb47d62fe4eb590ad5666a0
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-09-20 13:26:02 +0500

    Add: Uchburchak turi [mexliyevabdullo-art]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uchburchak_turi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uchburchak_turi.py
@@ -1,11 +1,12 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if a <= 0 or b <= 0 or c <= 0 or a + b <= c or a + b <= b or b + c <= a:
-    print("notogri")
-elif a == b == c:
-    print("teng tomonli")
-elif a == b or a == c or b == c:
-    print("teng yonli")
+if a > 0 and b > 0 and c > 0 and (a + b > c) and (a + b > c) and(b + c > a):
+    if a == b and b == c:
+        print("teng tomonli")
+    elif a == b or b == c or a == c:
+        print("teng yonli")
+    else:
+        print("turli tomonli")
 else:
-    print("turli tomonli")
+    print("notogri")
