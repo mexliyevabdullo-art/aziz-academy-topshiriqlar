dea7a87c76800f77f8dae51d657e1784ae69675d
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-09-20 13:03:23 +0500

    Add: Kabisa yili [mexliyevabdullo-art]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Kabisa_yili.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Kabisa_yili.py
@@ -1,5 +1,5 @@
 yil = int(input())
-if yil % 4 == 0 and (yil % 100 != 0 or yil % 400 == 0):
+if (yil % 4 == 0 and yil % 100 != 0) or (yil % 400 == 0):
     print("kabisa")
 else:
     print("oddiy")
