12d8e68040bc0cd576d791ee61f71642c1d996da
mexliyevabdullo-art <mexliyevabdullo@gmail.com>
2026-09-23 11:47:24 +0500

    Add: Elektr energiya to'lovini hisoblash [mexliyevabdullo-art]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Elektr_energiya_to_lovini_hisoblash.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Elektr_energiya_to_lovini_hisoblash.py
@@ -1,11 +1,9 @@
 kwh = int(input())
 if kwh < 0:
     print("Notogri qiymat")
+elif kwh <= 100:
+    print(kwh * 450)
+elif kwh <= 200:
+    print(100 * 450 + (kwh - 100) * 600)
 else:
-    if kwh <= 100:
-        tolov = kwh * 450
-    elif kwh <= 200:
-        tolov = 100 * 450 + (kwh - 100) * 600
-    else:
-        tolov = 100 * 450 + 100 * 600 + (kwh - 200) * 900
-    print(tolov)    
+    print(100 * 450 + 100 * 600 + (kwh - 200) * 900)
